ca0ca079b14f223c0f108abfc7528400b15ccca8
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-08-04 16:15:13 +0100

    Password checker

--- Program.cs
+++ Program.cs
@@ -6,9 +6,37 @@ using System;
 
 class Program
 {
-    static  void Main(string[] args) {
-        string name = "Aba";
-        int age = 30;
-        System.Console.WriteLine($"My name is {name} and i'm {age} years old");
+    static void Main(string[] args)
+    {
+        // string name = "Aba";
+        // int age = 30;
+        // System.Console.WriteLine($"My name is {name} and i'm {age} years old");
+        Console.Write("Enter Password: ");
+        string Password = Console.ReadLine()!;
+
+        Console.Write("Confirm Password: ");
+        string ConfirmPassword = Console.ReadLine()!;
+
+        Console.WriteLine(Password);
+        Console.WriteLine(ConfirmPassword);
+
+        if (!Password.Equals(string.Empty) && !ConfirmPassword.Equals(string.Empty))
+        {
+            if (Password == ConfirmPassword)
+            {
+                Console.WriteLine("Password Match");
+            }
+            else
+            {
+                Console.WriteLine("Password Do not Match");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please Enter Password: ");
+        }
+
+        Console.ReadLine();
+
     }
 }
--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694265dd142a7b27bb44db939d4352d66f4a543d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be16efca62d27963b19b8abdaa1d1cd0f98a9a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
