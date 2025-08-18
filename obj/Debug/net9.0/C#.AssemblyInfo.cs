17b942032e02834756ffd5d55c39c78fb3ce656c
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-08-18 09:26:42 +0100

    ready

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a87f657b9af7c701e190c29f0f43b5e428123d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b359017e9162fc68c3a7d0e383f6110de17781")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net9.0/es/C#.resources.cs
+++ obj/Debug/net9.0/es/C#.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f65d56e20abb8bb0b813a5eaa585f3895db5617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b359017e9162fc68c3a7d0e383f6110de17781")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
