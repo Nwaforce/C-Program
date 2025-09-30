79ba2f67dbe65028480d44943cac54f30d0ffea2
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-09-30 10:57:37 +0100

    yea

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d5e60ac4974175c1194d885f9d942b54c9b751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b523835add4749bdc64276af738ef1886ce93de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
